f6153dee1d2c3ab0c75d1cc8717fe88361e832bd
Yan Gabriel <yangabriel2.0.yg@gmail.com>
2026-08-29 11:33:32 -0300

    Criando a camada de  controller

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Request, Response, json } from "express";
+import { getPlayer } from "./controllers/players-controllers";
 
 function creatApp(){
     const app = express();
 
     app.use(express.json());
 
-    app.get("/", (req: Request, res: Response) => {
-        res.status(200).json({player:"Neymar"});
-    });
+    app.get("/", getPlayer);
     return app; 
 }
 
