bfea0b88e4cc31c66deb4f70c66fbe8d76703b9e
Yan Gabriel <yangabriel2.0.yg@gmail.com>
2026-08-29 11:52:13 -0300

    Criando a camada de Services.

--- src/controllers/players-controllers.ts
+++ src/controllers/players-controllers.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getPlayerService } from "../services/players-service";
 
-export const getPlayer = (req: Request, res: Response) => {
-        res.status(200).json({player:"Neymar Jr"});
+export const getPlayer = async (req: Request, res: Response) => {
+    const data = await getPlayerService();
+    res.status(200).json(data);
     }
---
+++ src/services/players-service.ts
@@ -0,0 +1,4 @@
+export const getPlayerService = async ()=>{
+    return {player: "Neymar Jr"};
+};
+
